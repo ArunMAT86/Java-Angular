e35227a772b24f5c35f5d64b25ba39affa7b6127
ArunMAT86 <arunmathew009@gmail.com>
2025-04-11 11:07:06 -0400

    added model class use and task

---
+++ BackendJava/src/main/java/com/revature/BackendJava/model/Task.java
@@ -0,0 +1,77 @@
+package com.revature.BackendJava.model;
+
+
+import jakarta.persistence.*;
+
+@Entity(name ="tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Task(String title, String description, User user) {
+        this.title = title;
+        this.description = description;
+        this.user = user;
+    }
+
+    public Task(Integer id, String title, String description, User user) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", user=" + user +
+                '}';
+    }
+}
---
+++ BackendJava/src/main/java/com/revature/BackendJava/model/User.java
@@ -0,0 +1,81 @@
+package com.revature.BackendJava.model;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true,nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user",fetch = FetchType.EAGER)
+    private List<Task> tasks = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String username, String password, List<Task> tasks) {
+        this.username = username;
+        this.password = password;
+        this.tasks = tasks;
+    }
+
+    public User(Integer id, String username, String password, List<Task> tasks) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.tasks = tasks;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", tasks=" + tasks +
+                '}';
+    }
+}
